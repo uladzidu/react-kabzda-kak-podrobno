c42850d3a6219ff0435522c4552c10e2e481e77d
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-10-25 13:52:26 +0200

    created a new file

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css'
+
+
+export function App() {
+    return (
+        <div>
+            Hey
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>
+            <div>star</div>
+        </div>
+    )
+}
+
+
+
