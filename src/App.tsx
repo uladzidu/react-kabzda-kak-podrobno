309f257b7753a86f009bcac215b1a73c13dfd100
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-10-30 16:24:38 +0200

    accordion collapsed

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,24 @@ export function App() {
     return (
         <div>
             {/* <PageTitle title="This is App component" />
-            <PageTitle title="This is App component 2" />
-            <Accordion title = "Menu" />
-            <Accordion title = "Users" /> */}
-            <Rating value={0} />
+            <PageTitle title="This is App component 2" /> */}
+            <Accordion title = "Menu" collapsed = {false} />
+            <Accordion title = "Users" collapsed = {true} />
+            {/* <Rating value={0} />
             <Rating value={1} />
             <Rating value={2} />
             <Rating value={3} />
             <Rating value={4} />
-            <Rating value={5} />
+            <Rating value={5} /> */}
         </div>
     )
 }
 
-function PageTitle(props: any) {
+function PageTitle(props: PagePropsType) {
     return <h1>{props.title}</h1>
+}
+
+
+type PagePropsType = {
+    title : string
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,15 +1,25 @@
 import React from "react"
 
 
+type AccordionPropsType = {
+    title : string
+    collapsed : boolean
+}
 
-
-export function Accordion(props : any) {
-    return (
+export function Accordion(props : AccordionPropsType) {
+    if (props.collapsed === true) {
+        return (
         <div>
             <AccordionTitle title = {props.title} />
             <AccordionBody />
         </div>
-    )
+    )} else {
+        return (
+            <div>
+                <AccordionTitle title = {props.title} />
+            </div>
+        )
+    }
 }
 
 function AccordionTitle(props : any) {
