f122b22f17f1be378a3d28c67232ffcbb5851035
Uladzislau Dukhounik <vdukhovnik1@gmail.com>
2021-12-16 19:57:49 +0100

    16.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 import {UncontrolledOnOff} from './components/OnOff/UncontrolledOnOff';
 import {UncontrolledAccordion} from './components/Accordion/UncontrolledAccordion';
 import {UncontrolledRating} from './components/Rating/UncontrolledRating';
 import {ControlledOnOff} from './components/OnOff/ControlledOnOff';
+import {Rating, RatingValueType} from './components/Rating/Rating';
 
+
+let [ratingValue, setRatingValue] = useState(4)
 
 export function App() {
     return (
-        <div className = 'AppClassName'>
+        <div className="AppClassName">
             <UncontrolledOnOff/>
-            <UncontrolledAccordion title='--Menu--'/>
+            <UncontrolledAccordion title="--Menu--"/>
             <UncontrolledRating/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
             <ControlledOnOff on={false}/>
         </div>
     )
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
-
-type RatingPropsType = {
+export type RatingValueType = {
     value: 0 | 1 | 2 | 3 | 4 | 5
-
 }
 
+type RatingPropsType = {
+    value: RatingValueType
+    onClick: (value: RatingValueType) => void
+}
 
 export function Rating(props: RatingPropsType) {
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
-
 type StarPropsType = {
     selected: boolean
+    onClick: (value: RatingValueType) => void
+    value: RatingValueType
 }
 
 export function Star(props: StarPropsType) {
