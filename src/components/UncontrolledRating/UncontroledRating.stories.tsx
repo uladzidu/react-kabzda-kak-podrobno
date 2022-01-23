82cc73e2ee5a697239527e83ff9de7230a02b9d0
master <vdukhovnik1@gmail.com>
2022-01-23 20:25:49 +0100

    23.01 Added storyBook. Some new examples with .stories.tsx

---
+++ src/components/UncontrolledRating/UncontroledRating.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {UncontrolledRating} from "./UncontrolledRating";
+
+export default {
+    title: 'UncontrolledRating',
+    component: UncontrolledRating
+};
+
+export const UncRating = () => <UncontrolledRating/>
+
+
+
+
+
